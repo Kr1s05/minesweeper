c6d66b6cd15d3c84f883d066d1ae1710472c31ad
Kr1s05 <kristianoreshkov04@gmail.com>
2024-10-29 23:55:28 +0200

    Got ui running

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::collections::{HashSet, VecDeque};
 use std::fmt::{Debug, Formatter};
-use std::time::Instant;
 use rand::Rng;
-use crate::Cell::{Discovered, Pin};
-use crate::Item::Empty;
+use crate::Cell::*;
+use crate::Item::*;
+use iced::{color, Element, Font, Theme};
+use iced::widget::{column, container, mouse_area, row, svg, text, MouseArea, Space};
+
+// svg::Handle::from_memory(include_bytes!("../resources/mine.svg").as_slice())
+
+
+// svg::Handle::from_memory(include_bytes!("../resources/flag.svg").as_slice())
+
 
 struct Game {
     field: Vec<Vec<Cell>>,
@@ -30,15 +37,55 @@ enum Cell {
     Pin(Item),
     Discovered(Item),
 }
+#[derive(Debug, Clone)]
+enum Message {}
 
 impl Cell {
     fn get_item(&self) -> Item {
         match self {
-            Cell::Hidden(item) | Pin(item) | Discovered(item) => item.clone()
+            Hidden(item) | Pin(item) | Discovered(item) => item.clone()
         }
     }
     fn turn_discovered(&self) -> Cell {
         Discovered(self.get_item())
+    }
+    fn to_button(&self) -> MouseArea<Message> {
+        let content = match self {
+            Hidden(_) => {
+                mouse_area(text("#")
+                    .width(20)
+                    .height(20)
+                )
+            }
+            Pin(_) => {
+                mouse_area(svg(svg::Handle::from_memory(include_bytes!("../resources/flag.svg").as_slice()))
+                    .width(20)
+                    .height(20)
+                )
+            }
+            Discovered(item) => {
+                match item {
+                    Bomb => {
+                        mouse_area(svg(svg::Handle::from_memory(include_bytes!("../resources/mine.svg").as_slice()))
+                            .width(20)
+                            .height(20)
+                        )
+                    }
+                    Empty(count) => {
+                        if *count == 0 {
+                            mouse_area(Space::new(20, 20))
+                        } else {
+                            println!("{}",count.to_string());
+                            mouse_area(text(count.to_string())
+                                .width(20)
+                                .height(20)
+                            )
+                        }
+                    }
+                }
+            }
+        };
+        content
     }
 }
 
@@ -48,7 +95,7 @@ impl Debug for Game {
         for row in &self.field {
             for cell in row {
                 match cell {
-                    Cell::Hidden(_) => {
+                    Hidden(_) => {
                         field_str.push_str(" _ ")
                     }
                     Pin(_) => {
@@ -56,7 +103,7 @@ impl Debug for Game {
                     }
                     Discovered(item) => {
                         match item {
-                            Item::Bomb => {
+                            Bomb => {
                                 field_str.push_str(" H ")
                             }
                             Empty(i) => {
@@ -74,11 +121,19 @@ impl Debug for Game {
     }
 }
 
+impl Default for Game {
+    fn default() -> Self { 
+        let mut g =  Game::new(16, 16);
+        let _ = &g.discover(&7,&7);
+        g
+    }
+}
+
 impl Game {
     fn new(width: usize, height: usize) -> Game {
         let mut rng = rand::thread_rng();
         let bomb_budget = width * height * 15 / 100;
-        let mut field = vec![vec![Cell::Hidden(Empty(0)); width]; height];
+        let mut field = vec![vec![Hidden(Empty(0)); width]; height];
         let mut bombs: HashSet<(usize, usize)> = HashSet::new();
         while bombs.len() < bomb_budget {
             let x = rng.gen_range(0..width);
@@ -92,8 +147,8 @@ impl Game {
                     for j in col_start..col_end + 1 {
                         let c = &mut field[i][j];
                         if i == y && j == x {
-                            *c = Cell::Hidden(Item::Bomb);
-                        } else if let Cell::Hidden(Empty(count)) = c {
+                            *c = Hidden(Bomb);
+                        } else if let Hidden(Empty(count)) = c {
                             *count += 1;
                         }
                     }
@@ -106,19 +161,19 @@ impl Game {
             state: GameState::Playing,
             bombs: bombs.into_iter().collect(),
             pins,
-            hidden_empty: (height*width - bomb_budget) as u32
+            hidden_empty: (height * width - bomb_budget) as u32,
         }
     }
 
     fn discover(&mut self, x: &usize, y: &usize) {
         let field = &mut self.field;
         match &field[*y][*x] {
-            Cell::Hidden(item) => {
+            Hidden(item) => {
                 match item {
-                    Item::Bomb => {
+                    Bomb => {
                         self.state = GameState::Loss;
                         for bomb in &self.bombs {
-                            self.field[bomb.1][bomb.0] = Discovered(Item::Bomb)
+                            self.field[bomb.1][bomb.0] = Discovered(Bomb)
                         }
                     }
                     Empty(score) => {
@@ -137,14 +192,14 @@ impl Game {
 
     fn pin(&mut self, x: usize, y: usize) {
         let cell = &mut self.field[y][x];
-        if let Cell::Hidden(_) = cell {
+        if let Hidden(_) = cell {
             *cell = Pin(cell.get_item());
-            self.pins.push((x,y));
+            self.pins.push((x, y));
         }
     }
 
-    fn update_game_state (&mut self) {
-        if self.hidden_empty  == 0 {
+    fn update_game_state(&mut self) {
+        if self.hidden_empty == 0 {
             self.state = GameState::Win;
         }
     }
@@ -154,7 +209,7 @@ fn fill_area(field: &mut Vec<Vec<Cell>>, start_row: usize, start_col: usize, hid
     let cols = field[0].len() as isize;
     let rows = field.len() as isize;
     let directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)];
-    let mut explored = vec![vec![false;cols as usize];rows as usize];
+    let mut explored = vec![vec![false; cols as usize]; rows as usize];
     let mut queue = VecDeque::new();
     queue.push_back((start_row, start_col));
     while let Some((row, col)) = queue.pop_front() {
@@ -171,8 +226,8 @@ fn fill_area(field: &mut Vec<Vec<Cell>>, start_row: usize, start_col: usize, hid
                     continue;
                 }
                 let (r, c) = (row as usize, col as usize);
-                if let Cell::Hidden(Empty(_)) = field[r][c] {
-                    if !explored[r][c]{
+                if let Hidden(Empty(_)) = field[r][c] {
+                    if !explored[r][c] {
                         queue.push_back((r, c));
                         explored[r][c] = true;
                     }
@@ -182,12 +237,23 @@ fn fill_area(field: &mut Vec<Vec<Cell>>, start_row: usize, start_col: usize, hid
     }
 }
 
-fn main() {
-    // println!("{:?}", &game);
-    // println!("{:?};{:?}", &game.bombs, &game.bombs.len());
-    let now = Instant::now();
-    let mut game = Game::new(30, 30);
-    let elapsed = now.elapsed();
-    game.discover(&8, &5);
-    println!("{:?}\n{:?}", &game, elapsed);
+fn update(game: &mut Game, message: Message) {}
+
+fn view(game: &Game) -> Element<Message> {
+    container(
+        column(
+            game.field.iter().map(|r| {
+                row(r.iter().map(|cell| cell.to_button().into())).into()
+            })
+        )
+    ).into()
+}
+
+fn main() -> iced::Result {
+    iced::application("Minesweeper",update,view)
+        .centered()
+        .resizable(false)
+        .window_size(iced::Size::new(600f32,800f32))
+        .theme(|_| Theme::Light)
+        .run()
 }
